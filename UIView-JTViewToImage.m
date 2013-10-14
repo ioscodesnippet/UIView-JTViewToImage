0931533683556f9c292028fdc8c29e89c6c75065
Jamz Tang <jamz@jamztang.com>
2013-10-14 18:51:53 +0800

    Update iOS 7 api

--- UIView-JTViewToImage.m
+++ UIView-JTViewToImage.m
@@ -1,5 +1,7 @@
 /*
  * This file is part of the http://ioscodesnippet.com
+ * http://ioscodesnippet.com/2011/08/25/rendering-any-uiviews-into-uiimage-in-one-line/
+ *
  * (c) Jamz Tang <jamz@jamztang.com>
  *
  * For the full copyright and license information, please view the LICENSE
@@ -11,17 +13,37 @@
 
 @implementation UIView (JTViewToImage)
 
+static BOOL _supportDrawViewHierarchyInRect;
+
++ (void)load {
+    if ([self instancesRespondToSelector:@selector(drawViewHierarchyInRect:afterScreenUpdates:)]) {
+        _supportDrawViewHierarchyInRect = YES;
+    } else {
+        _supportDrawViewHierarchyInRect = NO;
+    }
+}
+
 - (UIImage *)toImage {
     return [self toImageWithScale:0];
 }
 
 - (UIImage *)toImageWithScale:(CGFloat)scale {
+    UIImage *copied = [self toImageWithScale:scale legacy:NO];
+    return copied;
+}
+
+- (UIImage *)toImageWithScale:(CGFloat)scale legacy:(BOOL)legacy {
     // If scale is 0, it'll follows the screen scale for creating the bounds
     UIGraphicsBeginImageContextWithOptions(self.bounds.size, NO, scale);
     
-    // - [CALayer renderInContext:] also renders subviews 
-    [self.layer renderInContext:UIGraphicsGetCurrentContext()];
-
+    if (legacy || ! _supportDrawViewHierarchyInRect) {
+        // - [CALayer renderInContext:] also renders subviews
+        [self.layer renderInContext:UIGraphicsGetCurrentContext()];
+    } else {
+        [self drawViewHierarchyInRect:self.bounds
+                   afterScreenUpdates:YES];
+    }
+    
     // Get the image out of the context
     UIImage *copied = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
